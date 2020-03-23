fc626fbe6d9de5ffb4b74245f9e6d00b88b19cfa
akilaroha <akilaroha@hotmail.fr>
2020-03-23 20:52:43 +0100

    fonction iterative js

--- javascript/exercices-2/fonction_iterative.js
+++ javascript/exercices-2/fonction_iterative.js
@@ -1,8 +1,8 @@
-let factoriel = (nb) => {
+let factoriel_iterativ = (nb) => {
   let numberRef= 1;
   for (let i=1; i<=nb; i++){
   numberRef= numberRef*i;
   console.log(numberRef);
   }
 };
-console.log(factoriel(5));
+console.log(factoriel_iterativ(5));
