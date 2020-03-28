0d520aa93dd7312d01a619669ac3d12cfd58ecce
akilaroha <akilaroha@hotmail.fr>
2020-03-28 16:29:47 +0100

    resolution exercice iterative power

--- javascript/iterative_power.js
+++ javascript/iterative_power.js
@@ -1,14 +1,12 @@
 let iterative_power = (number, power) => {
-  for (let i = 0; i <= power; i++){/**compteur i = 0 pour arriver au même nombre que power**/
-    power = number * i ; /** à chaque boucle multiplier le compteur à number **/
-  }
-  if ( power < 0 ){ /**si puissance infèrieur à 0 retourner 0**/
+  let result= 1 ;
+  if (power <= 0){
     return 0;
+  } else { 
+    for (let i = 1 ; i <= power; i++){
+    result= result * number;
+    }
+return result;
   }
-};
-iterative_power(2, -1);/**appel de la fonction**/
-
-/**puissance d'un nombre (power)
-nombre au carré (number)
-nombre multiplié par lui même autant de fois que le nombre de sa puissance (number*number)*n_de_power;
-soit a4 = a*a*a*a **/
+}
+iterative_power(4,4);
