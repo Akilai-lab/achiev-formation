636acca6078f564f9c1503b22bf9c8fad26fedbf
akilaroha <akilaroha@hotmail.fr>
2020-03-29 13:04:05 +0200

    recursivité de recursive power

--- javascript/exercices-3/recursive_power.js
+++ javascript/exercices-3/recursive_power.js
@@ -1,8 +1,10 @@
 let recursive_power = (number, power) => {
-  if (power <= 0){
+  if(power === 0){
     return 0;
+  } else if (power === 1){
+    return number;
   } else {
-    return number ** power;
+    return number * recursive_power(number, power-1);
   }
-};
-console.log(recursive_power(4, 5));
+}
+recursive_power(3,4);
