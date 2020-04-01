2179d435b6caf8fc252d4bcdfcb4983809c043db
akilaroha <akilaroha@hotmail.fr>
2020-04-01 14:01:45 +0200

    resolution ex capitalize_word

---
+++ javascript/exercices-3/capitalize_word.js
@@ -0,0 +1,5 @@
+let capitalize_word = (string) => {
+  let first= string[0].toUpperCase()+string.substring(1,(3))+string[3].toUpperCase()+string.substring(4,(14));
+return first
+}
+capitalize_word("le coronavirus");
