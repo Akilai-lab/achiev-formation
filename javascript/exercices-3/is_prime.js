75aac2b0956f6e9460aafc4796adfb89ef430bdb
akilaroha <akilaroha@hotmail.fr>
2020-04-02 18:44:35 +0200

    resolution is prime

--- javascript/exercices-3/is_prime.js
+++ javascript/exercices-3/is_prime.js
@@ -1,10 +1,11 @@
 let is_prime = ( number ) => {
+  let result= 0;
   for (let i = 2; i < number; i++){
-       if( number%i === 0){
+    result = number % i;
+     if (result == 0){
       return false;
-    } else {
-      return true;
+      }
     }
-  }
+    return true;
 }
-is_prime(11);
+is_prime(45);
