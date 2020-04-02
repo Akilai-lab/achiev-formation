a4fe11d8ab4147f2cad05456a84f9b9d6439e690
akilaroha <akilaroha@hotmail.fr>
2020-04-02 18:49:25 +0200

    add let et indentation

--- javascript/exercices-3/next_prime.js
+++ javascript/exercices-3/next_prime.js
@@ -1,11 +1,9 @@
 let next_prime = (number) => {
-for (i = number; i => number; i++){
-    if(i%2 === 0 || i%3 === 0 || i%5 === 0){
-      console.log(i); 
-    } else {
-      console.log("nombre premier")
-      return i;
-    }
-  }
+	for (let i = number; i => number; i++){
+    	if(i % 2 === 0 || i % 3 === 0 || i % 5 === 0){ 
+    	} else {
+      		return i;
+    	}
+  	}
 }
 next_prime(20);
