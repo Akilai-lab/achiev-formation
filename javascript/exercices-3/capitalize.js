92e4a3b1ed5032f6836bf12fe811f81cfa2b0bc3
akilaroha <akilaroha@hotmail.fr>
2020-04-03 15:25:46 +0200

    resolution exercice capitalize après correction

--- javascript/exercices-3/capitalize.js
+++ javascript/exercices-3/capitalize.js
@@ -1,8 +1,10 @@
-/** chercher autrement **/
-
 let capitalize = (string) => {
-let first= string[0].toUpperCase()+string.substring(1,(6));
-return first
+  let result ="" ;
+  let first= string[0].toUpperCase ();
+  for (i = 1; i < string.length; i++){
+    result = result + string[i];
+    console.log(result);
+  }
+return first + result;
 }
-capitalize("nuages");
-
+capitalize("immeubles");
