733ab6c140b3fc91f728864c64d6b7e2bff41c51
akilaroha <akilaroha@hotmail.fr>
2020-04-04 13:13:22 +0200

    double fichier, un bon et un mauvais, celui ci est bon, j'ai supprimer l'autre

--- javascript/exercices-3/iterative_power.js
+++ javascript/exercices-3/iterative_power.js
@@ -1,11 +1,12 @@
-let iterative_power = (number, power) =>{
-	let result = " ";
-	if (power < 0){
-		return 0;
-	}
-	for (let i = 1; i <= power; i++){
-		result = power * i;
-	}
-  return result;
-};
-iterative_power(4, 5);
+let iterative_power = (number, power) => {
+  let result= 1 ;
+  if (power < 0){
+    return 0;
+  } else { 
+    for (let i = 1 ; i <= power; i++){
+    result= result * number;
+    }
+return result;
+  }
+}
+iterative_power(-2,-2);
