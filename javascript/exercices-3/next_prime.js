ba3cf458ba5f833e16d3140f05303553ea8821ef
akilaroha <akilaroha@hotmail.fr>
2020-04-05 17:38:45 +0200

    corrigé

--- javascript/exercices-3/next_prime.js
+++ javascript/exercices-3/next_prime.js
@@ -3,17 +3,17 @@ let is_prime = (number) => {
   for (let i = 2; i < number; i++) {
     result = number % i;
     if (result == 0) {
-      return false;
+    	return false;
     }
   }
   return true;
 }
 
 let next_prime = (number) => {
-    for (let i = number; i >= number; i++){
-      if (is_prime(i)){ 
+    for (let i = number; i >= number; i++) {
+      if (is_prime (i) ) { 
         return i;
-    	  } 
+    	} 
     }
 }
 next_prime(14);
