57be280bbada071622fbceaeed7c6ad4f3e31316
akilaroha <akilaroha@hotmail.fr>
2020-04-07 14:17:24 +0200

    indentation

--- javascript/exercices-3/iterative_power.js
+++ javascript/exercices-3/iterative_power.js
@@ -1,12 +1,12 @@
 let iterative_power = (number, power) => {
-  let result= 1 ;
-  if (power < 0){
-    return 0;
-  } else { 
-    for (let i = 1 ; i <= power; i++){
-    result= result * number;
-    }
-return result;
-  }
+  let result= 1;
+  	if (power < 0) {
+    	return 0;
+  	} else { 
+    	for (let i = 1 ; i <= power; i++) {
+    		result= result * number;
+    	}
+	return result;
+  	}
 }
 iterative_power(-2,-2);
