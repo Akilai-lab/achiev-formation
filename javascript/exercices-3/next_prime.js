ea4487c0d80570300e2688323d58ebadaaa4cfc9
akilaroha <akilaroha@hotmail.fr>
2020-04-07 14:17:55 +0200

    indentation

--- javascript/exercices-3/next_prime.js
+++ javascript/exercices-3/next_prime.js
@@ -2,16 +2,16 @@ let is_prime = (number) => {
   let result = 0;
   for (let i = 2; i < number; i++) {
     result = number % i;
-    if (result == 0) {
-    	return false;
-    }
+      if (result == 0) {
+    	 return false;
+      }
   }
   return true;
 }
 
 let next_prime = (number) => {
     for (let i = number; i >= number; i++) {
-      if (is_prime (i) ) { 
+      if (is_prime (i)) { 
         return i;
     	} 
     }
