e60c84ab8a9ca40627f870514fb109582820c7d0
akilaroha <akilaroha@hotmail.fr>
2020-04-07 13:59:10 +0200

    resolution ex avec resolution tirée de capitalize word

--- javascript/exercices-3/capitalize.js
+++ javascript/exercices-3/capitalize.js
@@ -1,9 +1,5 @@
 let capitalize = (string) => {
-  let result = " ";
-  let first = string[0].toUpperCase();
-  for (let i = 1; i < string.length; i++) {
-    result = result + string[i]; 
-  }
-return first + result;
+  let first = string[0].toUpperCase()+string.slice(1).toLowerCase();
+  return first;
 }
-capitalize("immeubles");
+capitalize("bulles");
