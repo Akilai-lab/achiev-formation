9683b779c76d39923d16582fdcf1648cc0500bcd
akilaroha <akilaroha@hotmail.fr>
2020-04-07 15:19:49 +0200

    espaces

--- javascript/exercices-3/fibonacci.js
+++ javascript/exercices-3/fibonacci.js
@@ -1,10 +1,10 @@
 let fibonacci = (index) => {
-    if (index === 0){
-    	return 0;
+    if (index === 0) {
+		return 0;
   	} else if (index === 1) {
-    	return 1;
+		return 1;
   	} else {
-    	return fibonacci ( index - 1 ) + fibonacci ( index - 2 );
+		return fibonacci(index - 1) + fibonacci(index - 2);
   	}
 };
 console.log(fibonacci(7));
