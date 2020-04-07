75a07291430898234ce187d061c256450208589a
akilaroha <akilaroha@hotmail.fr>
2020-04-07 15:20:42 +0200

    indentation

--- javascript/exercices-3/is_prime.js
+++ javascript/exercices-3/is_prime.js
@@ -1,11 +1,11 @@
 let is_prime = (number) => {
-  let result = 0;
-  for (let i = 2; i < number; i++) {
-    result = number % i;
-    	if (result == 0) {
-      		return false;
+	let result = 0;
+	for (let i = 2; i < number; i++) {
+		result = number % i;
+		if (result == 0) {
+			return false;
     	}
-  }
-  return true;
+	}
+	return true;
 }
 is_prime(45);
