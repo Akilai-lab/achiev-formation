044f96c0a77ba002530aceecac11e60be2fbe5e6
akilaroha <akilaroha@hotmail.fr>
2020-04-07 14:15:19 +0200

    appel de la fonction capitalize dans capitalize word

--- javascript/exercices-3/capitalize_word.js
+++ javascript/exercices-3/capitalize_word.js
@@ -1,6 +1,11 @@
 let capitalize_word = (string) => {
+    let capitalize = (string) => {
+      let first = string[0].toUpperCase()+string.slice(1).toLowerCase();
+      return first;
+  	}
+capitalize("bulles");
   return string.split (" ").map(function(chaine) {
-    return chaine.charAt(0).toUpperCase() + chaine.slice(1).toLowerCase();
-  }).join(" ");
+    return capitalize(chaine);
+  }	).join(" ");
 }
 capitalize_word("la plantation de haricots");
