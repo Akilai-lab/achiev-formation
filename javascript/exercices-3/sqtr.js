696bd0a60020d5f824370ddf321330306dcb384d
akilaroha <akilaroha@hotmail.fr>
2020-04-07 14:20:07 +0200

    indentation

--- javascript/exercices-3/sqtr.js
+++ javascript/exercices-3/sqtr.js
@@ -1,10 +1,10 @@
 let sqrt = (number) => {
   let result = 0;
-  for (let i = 1; i <= number; i++){
+  for (let i = 1; i <= number; i++) {
    	result = number / i;
-   	if(result * result === number){
-      return result;
-    }
+   	  if(result * result === number) {
+        return result;
+      }
   }
 }
 sqrt(49);
