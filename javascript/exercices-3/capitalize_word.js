b280b4d56b4c23b94e12d0e4039c67ab7b71f4b5
akilaroha <akilaroha@hotmail.fr>
2020-04-07 15:18:09 +0200

    indentation et espaces

--- javascript/exercices-3/capitalize_word.js
+++ javascript/exercices-3/capitalize_word.js
@@ -1,11 +1,11 @@
-let capitalize_word = (string) => {
-    let capitalize = (string) => {
-      let first = string[0].toUpperCase()+string.slice(1).toLowerCase();
-      return first;
-  	}
+let capitalize = (string) => {
+	return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
 capitalize("bulles");
-  return string.split (" ").map(function(chaine) {
-    return capitalize(chaine);
-  }	).join(" ");
+
+let capitalize_word = (string) => {
+	return string.split (" ").map(function(chaine) {
+    	return capitalize(chaine);
+													}).join(" ");
 }
 capitalize_word("la plantation de haricots");
