71eab26dfd26cd0e921c07dbfc55de74974af930
akilaroha <akilaroha@hotmail.fr>
2020-04-07 14:18:32 +0200

    indentation

--- javascript/exercices-3/recursive_power.js
+++ javascript/exercices-3/recursive_power.js
@@ -1,7 +1,7 @@
 let recursive_power = (number, power) => {
-  if(power <= 0){
+  if (power <= 0) {
     return 0;
-  } else if (power === 1){
+  } else if (power === 1) {
     return number;
   } else {
     return number * recursive_power(number, power-1);
