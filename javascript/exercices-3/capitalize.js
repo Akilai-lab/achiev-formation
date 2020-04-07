b604c92f5c1aac3976e168a76639cb77370a82b0
akilaroha <akilaroha@hotmail.fr>
2020-04-07 14:15:52 +0200

    indentation

--- javascript/exercices-3/capitalize.js
+++ javascript/exercices-3/capitalize.js
@@ -1,5 +1,5 @@
 let capitalize = (string) => {
-  let first = string[0].toUpperCase()+string.slice(1).toLowerCase();
-  return first;
+  let first = string[0].toUpperCase() + string.slice(1).toLowerCase();
+  	return first;
 }
 capitalize("bulles");
